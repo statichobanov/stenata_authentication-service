d8093b9529a8461ef1742146cd3b9796c0dc885a
Stati Chobanov <stati.chobanov@forkpoint.com>
2023-11-22 09:14:49 +0200

    Add refresh token interactor. Add middlewares

---
+++ middleware/AuthenticateToken.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const jwt = require("jsonwebtoken");
+const RefreshTokenInteractor = require("../useCases/RefreshTokenInteractor");
+const AuthInteractor = require("../useCases/AuthInteractor");
+
+async function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const accessToken = authHeader && authHeader.split(" ")[1];
+  const refreshToken = req.headers.cookie?.split("=")[1];
+  console.log("authenticateToken: ", accessToken, refreshToken);
+  const authInteractor = new AuthInteractor();
+  const refreshTokenInteractor = new RefreshTokenInteractor();
+
+  if (!accessToken || !refreshToken) {
+    return res.status(401).json({ message: "Missing Token" });
+  }
+
+  const decodedRefreshToken = jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET
+  );
+
+  const refreshTokenDBObject = await refreshTokenInteractor.findRefreshToken({
+    userId: decodedRefreshToken.sub,
+  });
+
+  if (!refreshTokenDBObject) {
+    return res.status(403).json({ message: "Invalid refresh token" });
+  }
+
+  if (
+    refreshTokenDBObject.token !== refreshToken ||
+    refreshTokenDBObject.expires < Date.now()
+  ) {
+    return res.status(401).json({ message: "Invalid refresh token" });
+  }
+
+  jwt.verify(
+    accessToken,
+    process.env.ACCESS_TOKEN_SECRET,
+    async (err, user) => {
+      if (err) {
+        if (err.name === "TokenExpiredError") {
+          const newAccessToken = await authInteractor.generateAccessToken({
+            id: decodedRefreshToken.sub,
+            username: decodedRefreshToken.username,
+          });
+
+          req.accessToken = newAccessToken;
+          req.user = {
+            id: decodedRefreshToken.sub,
+            username: decodedRefreshToken.username,
+          };
+          next();
+        } else {
+          return res.status(403).json({ message: "Invalid access token" });
+        }
+      } else {
+        req.user = user;
+        next();
+      }
+    }
+  );
+}
+
+module.exports = { authenticateToken };
--- useCases/RefreshTokenInteractor.js
+++ useCases/RefreshTokenInteractor.js
@@ -3,25 +3,31 @@
 
 const RefreshToken = require("../entities/RefreshToken");
 
-async function saveRefreshToken({ refreshToken, userId }) {
-  const newRefreshToken = new RefreshToken({
-    token: refreshToken,
-    userId: userId,
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000) /* One day */,
-  });
+class RefreshTokenInteractor {
+  async saveRefreshToken({ refreshToken, userId }) {
+    const newRefreshToken = new RefreshToken({
+      token: refreshToken,
+      userId: userId,
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000) /* One day */,
+    });
 
-  newRefreshToken.save();
-}
+    newRefreshToken.save();
+  }
 
-async function deleteRefreshToken({ userId }) {
-  await RefreshToken.findOneAndDelete({ userId: userId });
+  async deleteRefreshToken({ userId }) {
+    await RefreshToken.findOneAndDelete({ userId: userId });
 
-  /* just for info */
-  if (deletedToken) {
-    console.log(`Deleted existing token for userId: ${userId}`);
-  } else {
-    console.log(`No existing token found for userId: ${userId}`);
+    /* just for info */
+    if (deletedToken) {
+      console.log(`Deleted existing token for userId: ${userId}`);
+    } else {
+      console.log(`No existing token found for userId: ${userId}`);
+    }
+  }
+
+  async findRefreshToken({ userId }) {
+    return await RefreshToken.find({ userId: userId });
   }
 }
 
-module.exports = { saveRefreshToken, deleteRefreshToken };
+module.exports = RefreshTokenInteractor;
